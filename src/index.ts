57386708de79e14bbf6626a66811b9c7480356ca
Salman Mahmud <salmanmahmud2014@gmail.com>
2026-08-29 16:49:35 +0900

    Added all export functions

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { Presenter } from "./Presenter";
+export { decorate, decorateMany } from "./decorate";
+export { configurePresenter, getPresenterConfig } from "./config";
+export { createFormatAdapter } from "./format";
+
+export type {
+  AnyRecord,
+  FormatAdapter,
+  PresentOptions,
+  PresenterConstructor,
+  PresenterGlobalConfig,
+  ToJSONOptions,
+  TranslateFn,
+} from "./types";
